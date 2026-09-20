ec21b7c2d252c6008d268b2f0381980872d99b54
yastorovsky <306737236+yastorovsky@users.noreply.github.com>
2026-09-20 18:55:13 +0330

    feat(ui): remove Promotion option from top menu (v7.25.22)

--- v2rayN/v2rayN.Desktop/Views/MainWindow.axaml.cs
+++ v2rayN/v2rayN.Desktop/Views/MainWindow.axaml.cs
@@ -24,7 +24,6 @@ public partial class MainWindow : WindowBase<MainWindowViewModel>
 
         KeyDown += MainWindow_KeyDown;
         menuSettingsSetUWP.Click += MenuSettingsSetUWP_Click;
-        menuPromotion.Click += MenuPromotion_Click;
         menuCheckUpdate.Click += MenuCheckUpdate_Click;
         menuDownloadRelease.Click += (s, e) => ProcUtils.ProcessStart("https://github.com/yastorovsky/MehrN/releases/latest");
         btnNewUpdate.Click += MenuCheckUpdate_Click;
@@ -243,11 +242,6 @@ public partial class MainWindow : WindowBase<MainWindowViewModel>
                 ViewModel?.Reload();
             }
         }
-    }
-
-    private void MenuPromotion_Click(object? sender, RoutedEventArgs e)
-    {
-        ProcUtils.ProcessStart($"{Utils.Base64Decode(Global.PromotionUrl)}?t={DateTime.Now.Ticks}");
     }
 
     private void MenuSettingsSetUWP_Click(object? sender, RoutedEventArgs e)
--- v2rayN/v2rayN/Views/MainWindow.xaml.cs
+++ v2rayN/v2rayN/Views/MainWindow.xaml.cs
@@ -24,7 +24,6 @@ public partial class MainWindow
         Closing += MainWindow_Closing;
         PreviewKeyDown += MainWindow_PreviewKeyDown;
         menuSettingsSetUWP.Click += MenuSettingsSetUWP_Click;
-        menuPromotion.Click += MenuPromotion_Click;
         menuClose.Click += MenuClose_Click;
         menuCheckUpdate.Click += MenuCheckUpdate_Click;
         menuDownloadRelease.Click += (s, e) => ProcUtils.ProcessStart("https://github.com/yastorovsky/MehrN/releases/latest");
@@ -251,11 +250,6 @@ public partial class MainWindow
     {
         StorageUI();
         ShowHideWindow(false);
-    }
-
-    private void MenuPromotion_Click(object sender, RoutedEventArgs e)
-    {
-        ProcUtils.ProcessStart($"{Utils.Base64Decode(Global.PromotionUrl)}?t={DateTime.Now.Ticks}");
     }
 
     private void MenuSettingsSetUWP_Click(object sender, RoutedEventArgs e)
